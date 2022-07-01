b13dbd37ac41d1cd0639ebccfea909afc02caacd
Charles03131 <97250801+Charles03131@users.noreply.github.com>
2022-07-01 18:02:58 -0500

    Add files via upload

---
+++ findAndFindIndex/find-findindex.js
@@ -0,0 +1,41 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+
+function findUserByUsername(users, username) {
+  return users.find(function(user){
+ return user.username===username;
+  })
+
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+function removeUser(users, username) {
+  let theIndex=users.findIndex(function(user){
+    return user.username===username;
+  })
+  if(theIndex===-1) return;
+  return users.splice(theIndex,1)[0];
+}
+
